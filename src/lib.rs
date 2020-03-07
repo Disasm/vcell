58e80b837d45611e516d0aaf9a021f000885627f
Vadim Kaushan <admin@disasm.info>
2020-03-07 14:51:46 +0200

    Add VolatileCell32

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,31 @@
 #![deny(warnings)]
 #![no_std]
 
+extern crate avatar_common;
+
 use core::cell::UnsafeCell;
 use core::ptr;
+use avatar_common::{InfallibleMemoryInterface, StaticMemoryInterface};
+use core::sync::atomic::{AtomicPtr, Ordering};
+use core::ptr::null_mut;
+use core::ops::DerefMut;
+
+static INTERFACE: AtomicPtr<StaticMemoryInterface> = AtomicPtr::new(null_mut());
+
+fn memory_interface() -> Option<&'static mut dyn InfallibleMemoryInterface> {
+    let ptr = INTERFACE.load(Ordering::SeqCst);
+    if ptr.is_null() {
+        return None
+    }
+    unsafe {
+        Some((&mut *ptr).deref_mut())
+    }
+}
+
+/// Overrides the default memory interface
+pub fn set_memory_interface(interface: &'static mut StaticMemoryInterface) {
+    INTERFACE.store(interface, Ordering::SeqCst);
+}
 
 /// Just like [`Cell`] but with [volatile] read / write operations
 ///
@@ -44,6 +67,51 @@ impl<T> VolatileCell<T> {
     /// Returns a raw pointer to the underlying data in the cell
     #[inline(always)]
     pub fn as_ptr(&self) -> *mut T {
+        self.value.get()
+    }
+}
+
+/// Just like [`Cell`] but with [volatile] read / write operations
+///
+/// [`Cell`]: https://doc.rust-lang.org/std/cell/struct.Cell.html
+/// [volatile]: https://doc.rust-lang.org/std/ptr/fn.read_volatile.html
+pub struct VolatileCell32 {
+    value: UnsafeCell<u32>,
+}
+
+impl VolatileCell32 {
+    /// Creates a new `VolatileCell` containing the given value
+    pub const fn new(value: u32) -> Self {
+        VolatileCell32 { value: UnsafeCell::new(value) }
+    }
+
+    /// Returns a copy of the contained value
+    #[inline(always)]
+    pub fn get(&self) -> u32
+    {
+        if let Some(mem) = memory_interface() {
+            let address = self.value.get() as usize as u32;
+            mem.read32(address)
+        } else {
+            unsafe { ptr::read_volatile(self.value.get()) }
+        }
+    }
+
+    /// Sets the contained value
+    #[inline(always)]
+    pub fn set(&self, value: u32)
+    {
+        if let Some(mem) = memory_interface() {
+            let address = self.value.get() as usize as u32;
+            mem.write32(address, value)
+        } else {
+            unsafe { ptr::write_volatile(self.value.get(), value) }
+        }
+    }
+
+    /// Returns a raw pointer to the underlying data in the cell
+    #[inline(always)]
+    pub fn as_ptr(&self) -> *mut u32 {
         self.value.get()
     }
 }
